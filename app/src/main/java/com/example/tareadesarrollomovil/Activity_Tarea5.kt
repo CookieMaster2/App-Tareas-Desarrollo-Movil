f4caae068b954abca0bf03c9a20bd0b6ba1e5798
CookieMaster2 <danielsamvalla@gmail.com>
2024-04-09 17:08:40 -0600

    Cambios del time picker, añadido

--- app/src/main/java/com/example/tareadesarrollomovil/Activity_Tarea5.kt
+++ app/src/main/java/com/example/tareadesarrollomovil/Activity_Tarea5.kt
@@ -1,11 +1,89 @@
-package com.example.tareadesarrollomovil
-
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.TimePicker
+import com.example.tareadesarrollomovil.R
 
 class Activity_Tarea5 : AppCompatActivity() {
+
+    lateinit var previewSelectedTimeTextView: TextView
+
+    // listener which is triggered when the
+    // time is picked from the time picker dialog
+    private val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
+        object : TimePickerDialog.OnTimeSetListener {
+            override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+
+                // logic to properly handle
+                // the picked timings by user
+                val formattedTime: String = when {
+                    hourOfDay == 0 -> {
+                        if (minute < 10) {
+                            "${hourOfDay + 12}:0${minute} am"
+                        } else {
+                            "${hourOfDay + 12}:${minute} am"
+                        }
+                    }
+                    hourOfDay > 12 -> {
+                        if (minute < 10) {
+                            "${hourOfDay - 12}:0${minute} pm"
+                        } else {
+                            "${hourOfDay - 12}:${minute} pm"
+                        }
+                    }
+                    hourOfDay == 12 -> {
+                        if (minute < 10) {
+                            "${hourOfDay}:0${minute} pm"
+                        } else {
+                            "${hourOfDay}:${minute} pm"
+                        }
+                    }
+                    else -> {
+                        if (minute < 10) {
+                            "${hourOfDay}:${minute} am"
+                        } else {
+                            "${hourOfDay}:${minute} am"
+                        }
+                    }
+                }
+
+                previewSelectedTimeTextView.text = formattedTime
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tarea5)
+        setContentView(R.layout.activity_main)
+
+        // instance of the UI elements
+        val buttonPickTime: Button = findViewById<Button>(R.id.time_picker_button)
+        previewSelectedTimeTextView = findViewById<TextView>(R.id.time_picker_result)
+
+        // handle the pick time button to
+        // open the TimePickerDialog
+        buttonPickTime.setOnClickListener {
+            val timePicker: TimePickerDialog = TimePickerDialog(
+                // pass the Context
+                this,
+                // listener to perform task
+                // when time is picked
+                timePickerDialogListener,
+                // default hour when the time picker
+                // dialog is opened
+                12,
+                // default minute when the time picker
+                // dialog is opened
+                10,
+                // 24 hours time picker is
+                // false (varies according to the region)
+                false
+            )
+
+            // then after building the timepicker
+            // dialog show the dialog to user
+            timePicker.show()
+        }
     }
 }
