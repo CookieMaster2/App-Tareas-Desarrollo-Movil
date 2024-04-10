00e13e12316ac33593ef570c0797935b74a9abb0
Alejandro Rihani <rihanialex19@gmail.com>
2024-04-09 22:07:07 -0600

    subiendo cambios del date picker

--- app/src/main/java/com/example/tareadesarrollomovil/Activity_Tarea5.kt
+++ app/src/main/java/com/example/tareadesarrollomovil/Activity_Tarea5.kt
@@ -1,3 +1,6 @@
+package com.example.tareadesarrollomovil
+
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -5,11 +8,63 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.TimePicker
 import com.example.tareadesarrollomovil.R
+import java.util.Calendar
 
 class Activity_Tarea5 : AppCompatActivity() {
 
     lateinit var previewSelectedTimeTextView: TextView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tarea5)
+
+        // instance of the UI elements
+        val buttonPickTime: Button = findViewById<Button>(R.id.time_picker_button)
+        previewSelectedTimeTextView = findViewById<TextView>(R.id.time_picker_result)
+
+        // handle the pick time button to
+        // open the TimePickerDialog
+        buttonPickTime.setOnClickListener {
+            val timePicker: TimePickerDialog = TimePickerDialog(
+                // pass the Context
+                this,
+                // listener to perform task
+                // when time is picked
+                timePickerDialogListener,
+                // default hour when the time picker
+                // dialog is opened
+                12,
+                // default minute when the time picker
+                // dialog is opened
+                10,
+                // 24 hours time picker is
+                // false (varies according to the region)
+                false
+            )
+
+            // then after building the timepicker
+            // dialog show the dialog to user
+            timePicker.show()
+        }
+
+        val btnShowDatePicker: Button = findViewById(R.id.btnShowDatePicker)
+        val tvSelectedDate: TextView = findViewById(R.id.tvSelectedDate)
+
+        btnShowDatePicker.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog =
+                DatePickerDialog(this, { _, selectedYear, selectedMonth, dayOfMonth ->
+                    // Display the selected date on the TextView
+                    val selectedDate = "${selectedMonth + 1}/$dayOfMonth/$selectedYear"
+                    tvSelectedDate.text = selectedDate
+                }, year, month, day)
+
+            datePickerDialog.show()
+        }
+    }
     // listener which is triggered when the
     // time is picked from the time picker dialog
     private val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
@@ -53,37 +108,5 @@ class Activity_Tarea5 : AppCompatActivity() {
             }
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // instance of the UI elements
-        val buttonPickTime: Button = findViewById<Button>(R.id.time_picker_button)
-        previewSelectedTimeTextView = findViewById<TextView>(R.id.time_picker_result)
-
-        // handle the pick time button to
-        // open the TimePickerDialog
-        buttonPickTime.setOnClickListener {
-            val timePicker: TimePickerDialog = TimePickerDialog(
-                // pass the Context
-                this,
-                // listener to perform task
-                // when time is picked
-                timePickerDialogListener,
-                // default hour when the time picker
-                // dialog is opened
-                12,
-                // default minute when the time picker
-                // dialog is opened
-                10,
-                // 24 hours time picker is
-                // false (varies according to the region)
-                false
-            )
-
-            // then after building the timepicker
-            // dialog show the dialog to user
-            timePicker.show()
-        }
-    }
 }
